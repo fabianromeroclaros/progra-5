d9e6006659e03cf21d7d855c2d87b3a818c5c9b7
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2024-02-26 00:41:08 -0400

    feat: Assignment Calular Numeros Primos

---
+++ assignments/assignment_02_27_24/numerosPrimos/NumerosPrimos.hs
@@ -0,0 +1,14 @@
+module NumerosPrimos where
+
+numPrimos::Int -> [Int] 
+numPrimos n = filter esPrimo [1..n]
+
+esPrimo :: Int -> Bool
+esPrimo 1 = False
+esPrimo n = esPrimoAux n 2
+
+esPrimoAux :: Int -> Int -> Bool
+esPrimoAux n divisor
+    | divisor * divisor > n = True
+    | mod n divisor == 0 = False
+    | otherwise = esPrimoAux n (divisor + 1)
